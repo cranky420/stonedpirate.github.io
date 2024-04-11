4ad1f8ce3ecabe474afeec669db10baa065bc299
bijay <brsinghdeo@gmail.com>
2024-04-11 20:54:47 +0530

    added ebnagli fonts for rohit

--- app.py
+++ app.py
@@ -13,11 +13,14 @@ def play():
     secret_number = random.randint(1, 100)
     guess = int(request.form['guess'])
     if guess == 69:  # Check if the guessed number is 69
-        result = "Cheater! You used the cheat code! Congratulations!"
+        result = "প্রতারক ! আপনি চিট কোড ব্যবহার করেছেন! অভিনন্দন!"
+        # Cheater! You used the cheat code! Congratulations!
     elif guess < secret_number:
-        result = "Too low! Try again."
+        result = "অনেক কম! আবার চেষ্টা কর."
+        # Too low! Try again.
     elif guess > secret_number:
-        result = "Too high! Try again."
+        result = "খুব উচ্চ! আবার চেষ্টা কর."
+        # Too high! Try again.
     else:
         result = f"Congratulations! You're a true Genius, you guessed the seceret number {secret_number}!"
     return render_template('result.html', result=result)
