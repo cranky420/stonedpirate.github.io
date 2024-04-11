952e098b982c87998a330f40647ce0d38d43bbc2
bijay <brsinghdeo@gmail.com>
2024-04-11 21:02:01 +0530

    bengali fonts invalid

--- app.py
+++ app.py
@@ -13,14 +13,11 @@ def play():
     secret_number = random.randint(1, 100)
     guess = int(request.form['guess'])
     if guess == 69:  # Check if the guessed number is 69
-        result = "প্রতারক ! আপনি চিট কোড ব্যবহার করেছেন! অভিনন্দন!"
-        # Cheater! You used the cheat code! Congratulations!
+        result = "Cheater! You used the cheat code! Congratulations!"
     elif guess < secret_number:
-        result = "অনেক কম! আবার চেষ্টা কর."
-        # Too low! Try again.
+        result = "Too low! Try again."
     elif guess > secret_number:
-        result = "খুব উচ্চ! আবার চেষ্টা কর."
-        # Too high! Try again.
+        result = "Too high! Try again."
     else:
         result = f"Congratulations! You're a true Genius, you guessed the seceret number {secret_number}!"
     return render_template('result.html', result=result)
